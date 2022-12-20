2ef534d6960c3a2c3d33efca50afb5b4e5aea55b
halanaman <81603736+halanaman@users.noreply.github.com>
2022-12-20 14:05:10 +0800

    Update get_receipt.py

--- get_receipt.py
+++ get_receipt.py
@@ -1,10 +1,15 @@
 # Tkinter GUI Interface
 from tkinter import *
+from tkinter import messagebox
 
 from get_rfid import *
 from firebase import *
 
 val = {"User":"Tap Your Card", "Receipts":[]}
+
+def filtering(mode):
+    if mode == "e-receipts":
+        display()
 
 # Create frame_display which is destroyable for updating the informations to display
 # Display all the information as individual buttons after filtering
@@ -16,7 +21,7 @@ def display():
     frame_display.pack()
 
     # Using canvas to use scrollbar
-    canvas = Canvas(frame_display, width=635, height=500)
+    canvas = Canvas(frame_display, width=235, height=500)
 
     vsb = Scrollbar(frame_display, orient="vertical", command=canvas.yview)
     hsb = Scrollbar(frame_display, orient="horizontal", command=canvas.xview)
@@ -28,7 +33,7 @@ def display():
     vsb.grid(row=0, column=1, sticky="ns")
     hsb.grid(row=1, column=0, sticky="ew")
 
-    label_display = Label(frame_display, text=val["User"], font=("Arial", 8, "bold"))
+    label_display = Label(frame_display, text="Transaction History", font=("Arial", 8, "bold"))
     canvas.create_window(50, 0, anchor="nw", window=label_display)
 
     # Initialize positions
@@ -42,45 +47,76 @@ def display():
         # This function is used to keep track of each key as every button has its own unique key
         def func(x=key):
             return display2(x)
-        dict_btn[key] = Button(canvas, text="Receipt No. {}".format(key+1), width=15, height=4, command=func)
+
+        t = val["Receipts"][key].split("\n")
+        shop_name = t[0].strip(" -Welcome to")
+        shop_total = t[len(t)-1].strip("Your Total bill amount is :")
+
+        dict_btn[key] = Button(canvas, text=shop_name+"   "+shop_total, width=30, height=2, command=func)
         #dict_btn[key].bind('<FocusIn>', lambda event: (dict_btn[key].configure(bg="cyan")))
 
         # Place each button into its positions assigned
         canvas.create_window(i, j, anchor="nw", window=dict_btn[key])
-        j+=75
+        j+=45
 
     canvas.bind("<Configure>", lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
 
 def display2(key):
-    global window3, val
-    window3.destroy()
-    window3 = Toplevel(window)
-    window3.title("Receipt No. {}".format(key+1))
-    frame3 = Frame(window3)
-    frame3.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+    global window3, frame_display2, val
+    frame_display2.destroy()
+    frame_display2 = Frame(window3)
+    frame_display2.pack()
+
+    t = val["Receipts"][key].split("\n")
+    shop_name = t[0].strip(" -Welcome to")
+    shop_total = t[len(t)-1].strip("Your Total bill amount is :")
 
     # Display more information about the planning
-    label_31 = Label(window3, text=val["Receipts"][key], font=("Arial", 8, "bold"))
+    label_30 = Label(frame_display2, text=shop_name+"   "+shop_total, font=("Arial", 8, "bold"))
+    label_31 = Label(frame_display2, text=val["Receipts"][key], font=("Arial", 8, "bold"))
+    label_30.grid(row=0, column=0, padx=5, pady=5, sticky='w')
     label_31.grid(row=1, column=0, padx=5, pady=5, sticky='w')
 
 def reload():
-    global val
+    global val, label_filtering, frame_display, frame_display2
     rfid,name = get_values()
     if rfid != "":
         #userdata = {"User":name, "Receipts":generate_receipt()}
         #set_receipt(rfid,userdata)
-        
-        val = dict(get_receipt(rfid))
-        display()
-    
+        try:
+            val = dict(get_receipt(rfid))
+            label_filtering['text'] = "Hello,\n"+val["User"]
+            frame_display.destroy()
+            frame_display2.destroy()
+        except:
+            messagebox.showwarning("Warning", "Unknown User has no History")
+
     window.after(100, reload)
-    
+
 ##### App Design #####
 
 # Creating Window ############################################################################
 window = Tk()
 window.title('E-Receipt')
-window.geometry("250x540")
+window.geometry("600x540")
+
+# Create Frame 1: Filter Buttons #############################################################
+frame1 = Frame(window)
+frame1.pack(side=LEFT, anchor=NW)
+
+# Filter buttons
+label_filtering = Label(frame1, text="Please Tap Your Card", font=("Arial", 8, "bold"))
+btn_pax = Button(frame1, text="E-Receipts", font=("Arial", 8, "bold"), width=9, height=4, command=lambda: filtering('e-receipts'))
+btn_occasion = Button(frame1, text="Refunds", font=("Arial", 8, "bold"), width=9, height=4, command=lambda: filtering('refunds'))
+btn_location = Button(frame1, text="Rewards", font=("Arial", 8, "bold"), width=9, height=4, command=lambda: filtering('rewards'))
+btn_time = Button(frame1, text="Report", font=("Arial", 8, "bold"), width=9, height=4, command=lambda: filtering('report'))
+
+label_filtering.grid(row=0, column=0, padx=5, pady=5)
+btn_pax.grid(row=1, column=0, padx=5, pady=5)
+btn_occasion.grid(row=2, column=0, padx=5, pady=5)
+btn_location.grid(row=3, column=0, padx=5, pady=5)
+btn_time.grid(row=4, column=0, padx=5, pady=5)
+# ############################################################################################
 
 # Create Frame 2: Display Planning ###########################################################
 frame2 = Frame(window)
@@ -91,8 +127,10 @@ display()
 # ############################################################################################
 
 # Window 3: Event Description ################################################################
-window3 = Toplevel(window)
-window3.destroy()
+window3 = Frame(window)
+window3.pack(side=LEFT, anchor=NE)
+frame_display2 = Frame(window3)
+frame_display2.pack()
 # ############################################################################################
 
 reload()
