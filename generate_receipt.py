fa52eae43a51b8c59f3138ec62503dd3796d7c38
halanaman <81603736+halanaman@users.noreply.github.com>
2022-12-20 14:03:02 +0800

    Update generate_receipt.py

--- generate_receipt.py
+++ generate_receipt.py
@@ -1,37 +1,80 @@
 import random
 
 def receipt():
-    title = """--------------WELCOME TO ABC SHOP--------------
+    
+    shops = [
+                {"name":"ABC_Shop",
+                "Shampoo":9.9, "Canola Oil":7.5, 
+                "Thai Rice":12.1, "Meji Crackers":5.7,
+                "Chocolate":2.95, "Chips":1.95,
+                "Soft Drink":2.05, "Coffee":4.95,
+                "Tea":3.95, "Chilli Sauce":6.8,
+                "Mask":19.9, "Milo Packets":14.8,
+                "Tissue":3.55, "Hand Soap":3.9,
+                "Toothpaste":6.9, "Milk":5.95
+                },
+                
+                {"name":"NTUC",
+                "Shampoo":9.9, "Canola Oil":7.5, 
+                "Thai Rice":12.1, "Meji Crackers":5.7,
+                "Chocolate":2.95, "Chips":1.95,
+                "Soft Drink":2.05, "Coffee":4.95,
+                "Tea":3.95, "Chilli Sauce":6.8,
+                "Mask":19.9, "Milo Packets":14.8,
+                "Tissue":3.55, "Hand Soap":3.9,
+                "Toothpaste":6.9, "Milk":5.95
+                },
+                
+                {"name":"Cold Storage",
+                "Shampoo":9.9, "Canola Oil":7.5, 
+                "Thai Rice":12.1, "Meji Crackers":5.7,
+                "Chocolate":2.95, "Chips":1.95,
+                "Soft Drink":2.05, "Coffee":4.95,
+                "Tea":3.95, "Chilli Sauce":6.8,
+                "Mask":19.9, "Milo Packets":14.8,
+                "Tissue":3.55, "Hand Soap":3.9,
+                "Toothpaste":6.9, "Milk":5.95
+                },
+                
+                {"name":"Sheng Siong",
+                "Shampoo":9.9, "Canola Oil":7.5, 
+                "Thai Rice":12.1, "Meji Crackers":5.7,
+                "Chocolate":2.95, "Chips":1.95,
+                "Soft Drink":2.05, "Coffee":4.95,
+                "Tea":3.95, "Chilli Sauce":6.8,
+                "Mask":19.9, "Milo Packets":14.8,
+                "Tissue":3.55, "Hand Soap":3.9,
+                "Toothpaste":6.9, "Milk":5.95
+                }
+            ]
+    
+    i = random.randint(0,3)
+    
+    top = shops[i]["name"]
+    top = "Welcome_to_" + top
+    top = "{:^47s}".format(top)
+    top = top.replace(" ","-").replace("_"," ")
+    title = """{}
 +---------------------------------------------+
        Item Name       |     Item Price($)     
                        |                       
-"""
+""".format(top)
 
-    items = {
-            "Shampoo":9.9, "Canola Oil":7.5, 
-            "Thai Rice":12.1, "Meji Crackers":5.7,
-            "Chocolate":2.95, "Chips":1.95,
-            "Soft Drink":2.05, "Coffee":4.95,
-            "Tea":3.95, "Chilli Sauce":6.8,
-            "Mask":19.9, "Milo Packets":14.8,
-            "Tissue":3.55, "Hand Soap":3.9,
-            "Toothpaste":6.9, "Milk":5.95
-            }
-
-    keys = list(items.keys())
+    keys = list(shops[i].keys())
+    keys.remove("name")
     random.shuffle(keys)
-    keys = keys[:random.randint(1,len(items))]
+    keys = keys[:random.randint(1,len(shops[i]))]
 
     receipt = title
     total = 0
     for k in keys:
-        receipt += "{:^23s}|{:^23.2f}\n".format(k,items[k])
-        total += items[k]
+        receipt += "{:^23s}|{:^23.2f}\n".format(k,shops[i][k])
+        total += shops[i][k]
 
     receipt += "+---------------------------------------------+\n"
     receipt += "{:^23s}|{:^23.2f}\n".format("Total",total)
     receipt += """\nThanks for shopping with us :)
-Your Total bill amount is Total: ${:.2f}""".format(total)
+Your Total bill amount is : ${:.2f}""".format(total)
     return receipt
     
 def generate_receipt():
